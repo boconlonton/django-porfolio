05eab960ab51dbeb28d3be6633870102072efefb
tantruong <truongnguyenhoangtan@gmail.com>
2021-04-10 15:43:18 +0700

    Add blog and running it up

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,18 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField()
+    summary = models.CharField(max_length=250, null=True)
     # upload_to requires a relative path
     image = models.ImageField(upload_to='images/blog')
+
+    def description(self):
+        return self.body[:100] + '...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return f'{self.title}'
 
 # Add the blog app to settings
 
